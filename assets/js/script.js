9add1f33a1469d69e500f86c00a69aeb3913d882
Max M1nt <maks-foxf1@yandex.ru>
2019-05-15 20:09:27 +0300

    v1.0

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+	// Preloader
+	$(window).on('load', function () {
+		$('.preloader-background').delay(1500).fadeOut('slow');
+	});
+
+	// Sidenav
+	$('.sidenav').sidenav();
+
+	// Carousel
+	$('.carousel.carousel-slider').carousel({
+		fullWidth: true,
+		indicators: true
+	});
+
+	// Parallax
+	$('.parallax').parallax();
+
+	// Anchor
+	$('a[href^="#"]').on('click', function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
+
+});
